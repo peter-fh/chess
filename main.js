d4f73a372a66ae24220a0dbc6065a63f0afa538c
peterfh <p.felty.howe@gmail.com>
2024-07-23 01:40:29 -0400

    added fen functionality to chess class

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 "use strict";
+var PieceTypes = ['K', 'Q', 'R', 'B', 'N', 'P', 'k', 'q', 'r', 'b', 'n', 'p'];
+var Sides = ['w', 'b'];
+function isPiece(c) {
+    return PieceTypes.indexOf(c) != -1;
+}
 var Piece = /** @class */ (function () {
-    function Piece(type, side, has_moved) {
+    function Piece(type, has_moved) {
         this.type = type;
-        this.side = side;
+        this.side = this.getPieceColor(type);
         this.has_moved = has_moved;
     }
     Piece.prototype.toString = function () {
         return "".concat(this.type);
+    };
+    Piece.prototype.getPieceColor = function (piece) {
+        if (piece.toLowerCase() == piece) {
+            return 'b';
+        }
+        return 'w';
     };
     return Piece;
 }());
@@ -16,24 +27,65 @@ var Board = /** @class */ (function () {
         this.initializeFromFen(fen);
     }
     Board.prototype.initializeFromFen = function (fen) {
-        this.board.fill(null);
+        this.board.fill(' ');
         var i = 0;
         for (var _i = 0, fen_1 = fen; _i < fen_1.length; _i++) {
             var c = fen_1[_i];
-            try {
-                console.log(c);
-                console.log("yes");
+            if (isPiece(c)) {
+                var current_piece = new Piece(c, false);
+                this.board[i] = current_piece;
+                i++;
             }
-            catch (_a) {
-                console.log("no");
+            else if (Number.isInteger(Number(c))) {
+                i += parseInt(c);
+            }
+            else if (c === ' ') {
+                break;
             }
         }
     };
     Board.prototype.fen = function () {
-        return "";
+        var board_fen = "";
+        var space_counter = 0;
+        for (var i = 0; i < 64; ++i) {
+            if (i % 8 == 0 && i != 0) {
+                if (space_counter > 0) {
+                    board_fen += space_counter;
+                    space_counter = 0;
+                }
+                board_fen += "/";
+            }
+            var c = this.board[i];
+            if (c instanceof Piece) {
+                if (space_counter > 0) {
+                    board_fen += space_counter;
+                    space_counter = 0;
+                }
+                board_fen += c.toString();
+            }
+            else if (c == ' ') {
+                space_counter++;
+            }
+            else {
+                throw new Error("board has non-piece non-space element");
+            }
+        }
+        return board_fen;
+    };
+    Board.prototype.toString = function () {
+        var str = "";
+        for (var i = 0; i < 8; ++i) {
+            // feast your eyes on this beautiful one liner
+            str += this.board.slice(i * 8, (i + 1) * 8).map(String).join(' ');
+            str += "\n";
+        }
+        return "".concat(str);
     };
     return Board;
 }());
-var starting_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+var starting_fen_full = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+var starting_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
 var board = new Board(starting_fen);
+console.log('' + board);
+console.log(board.fen());
 console.assert(board.fen() === starting_fen);
--- main.ts
+++ main.ts
@@ -3,6 +3,10 @@ const Sides = ['w', 'b'] as const;
 type PieceType = typeof PieceTypes[number];
 type Side = typeof Sides[number];
 
+function isPiece(c: string){
+    return PieceTypes.indexOf(c as PieceType) != -1;
+}
+
 
 
 class Piece {
@@ -28,7 +32,7 @@ class Piece {
 }
 
 
-type BoardArray = (Piece | null)[];
+type BoardArray = (Piece | ' ')[];
 class Board {
     board: BoardArray;
 
@@ -38,23 +42,66 @@ class Board {
     }
 
     initializeFromFen(fen: string){
-	this.board.fill(null);
+	this.board.fill(' ');
 	var i: number = 0;
 	for (const c of fen){
-	    if (c in PieceTypes) {
+	    if (isPiece(c)) {
 		const current_piece = new Piece(c as PieceType, false);
 		this.board[i] = current_piece;
 		i++;
+	    } else if (Number.isInteger(Number(c))) {
+		i += parseInt(c);
+	    } else if (c === ' ') {
+		break;
 	    }
 	}
     }
 
     fen(): string {
-	return "";
+	var board_fen: string = "";
+	var space_counter: number = 0;
+	for(var i: number = 0; i < 64; ++i){
+	    if (i % 8 == 0 && i != 0){
+		if (space_counter > 0) {
+		    board_fen += space_counter;
+		    space_counter = 0;
+		}
+		board_fen += "/"
+	    }
+	    var c: (Piece | ' ') = this.board[i];
+	    if (c instanceof Piece) {
+		if (space_counter > 0) {
+		    board_fen += space_counter;
+		    space_counter = 0;
+		}
+		board_fen += c.toString();
+	    } else if (c == ' ') {
+		space_counter++
+	    } else {
+		throw new Error("board has non-piece non-space element");
+	    }
+	}
+	return board_fen;
+    }
+
+    public toString() {
+	
+	var str: String = ""
+	for (var i: number = 0; i < 8; ++i){
+	    // feast your eyes on this beautiful one liner
+	    str += this.board.slice(i*8, (i+1) * 8).map(String).join(' ');
+	    str += "\n"
+	}
+	return `${str}`;
     }
 }
 
 
-const starting_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+
+
+const starting_fen_full = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+const starting_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
 const board = new Board(starting_fen);
+console.log('' + board);
+console.log(board.fen())
 console.assert(board.fen() === starting_fen);
