991e48d86ec4f02d89c9a73f6d633a57a7c83682
peterfh <p.felty.howe@gmail.com>
2024-07-23 02:39:22 -0400

    drawing board

--- main.js
+++ main.js
@@ -83,9 +83,57 @@ var Board = /** @class */ (function () {
     };
     return Board;
 }());
+function drawBoard(board) {
+    var _a;
+    var board_div = document.getElementById("board");
+    if (board_div == null) {
+        return;
+    }
+    for (var i = 0; i < 64; ++i) {
+        var square = document.createElement("div");
+        square.id = "square";
+        var square_class_list = ["square"];
+        if (Math.floor(i / 8) % 2 == 0) {
+            if (i % 2 == 0) {
+                square_class_list.push("light");
+            }
+            else {
+                square_class_list.push("dark");
+            }
+        }
+        else {
+            if (i % 2 == 1) {
+                square_class_list.push("light");
+            }
+            else {
+                square_class_list.push("dark");
+            }
+        }
+        (_a = square.classList).add.apply(_a, square_class_list);
+        if (i == 0) {
+            square.style.borderRadius = "10px 0px 0px 0px";
+        }
+        else if (i == 7) {
+            square.style.borderRadius = "0px 10px 0px 0px";
+        }
+        else if (i == 56) {
+            square.style.borderRadius = "0px 0px 0px 10px";
+        }
+        else if (i == 63) {
+            square.style.borderRadius = "0px 0px 10px 0px";
+        }
+        board_div.append(square);
+        if ((i + 1) % 8 == 0) {
+            var break_div = document.createElement("div");
+            break_div.id = "break";
+            board_div.append(break_div);
+        }
+    }
+}
 var starting_fen_full = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 var starting_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
 var board = new Board(starting_fen);
 console.log('' + board);
 console.log(board.fen());
 console.assert(board.fen() === starting_fen);
+drawBoard(board);
--- main.ts
+++ main.ts
@@ -98,6 +98,53 @@ class Board {
 
 
 
+function drawBoard(board: Board) {
+    const board_div = document.getElementById("board");
+    if (board_div == null) {
+	return;
+    }
+    for (let i=0; i < 64; ++i){
+	const square = document.createElement("div");
+	square.id = "square";
+
+	var square_class_list = ["square"]
+
+	if (Math.floor(i / 8) % 2 == 0) {
+	    if (i % 2 == 0) {
+		square_class_list.push("light");
+	    } else {
+		square_class_list.push("dark");
+	    }
+	} else {
+	    if (i % 2 == 1) {
+		square_class_list.push("light");
+	    } else {
+		square_class_list.push("dark");
+	    }
+	}
+
+
+	square.classList.add(...square_class_list);
+	if (i == 0) {
+	    square.style.borderRadius = "10px 0px 0px 0px";    
+	} else if (i == 7) {
+	    square.style.borderRadius = "0px 10px 0px 0px";    
+	} else if (i == 56) {
+	    square.style.borderRadius = "0px 0px 0px 10px";    
+	} else if (i == 63) {
+	    square.style.borderRadius = "0px 0px 10px 0px";    
+	}
+
+	board_div.append(square);
+	if ((i + 1) % 8 == 0) {
+	    const break_div = document.createElement("div");
+	    break_div.id = "break";
+	    board_div.append(break_div);
+	}
+
+    }
+
+}
 
 const starting_fen_full = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 const starting_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
@@ -105,3 +152,5 @@ const board = new Board(starting_fen);
 console.log('' + board);
 console.log(board.fen())
 console.assert(board.fen() === starting_fen);
+
+drawBoard(board);
